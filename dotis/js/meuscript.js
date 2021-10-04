956596e8b43b207ffe90e4c5345d90c1ade5a78c
Ing <82782011+ingguk@users.noreply.github.com>
2021-10-04 10:28:41 -0300

    Add files via upload

--- dotis/js/meuscript.js
+++ dotis/js/meuscript.js
@@ -1,45 +1,47 @@
-$(function() { // quando o documento estiver pronto/carregado
-    
+$(function () { // quando o documento estiver pronto/carregado
+
     $.ajax({
         url: 'http://localhost:5000/listar_pets',
         method: 'GET',
         dataType: 'json', // os dados são recebidos no formato json
         success: listar, // chama a função listar para processar o resultado
-        error: function() {
+        error: function () {
             alert("erro ao ler dados, verifique o backend");
         }
     });
 
-    function listar (pets) {
+    function listar(pets) {
         // percorrer a lista de pessoas retornadas; 
         for (var i in pets) { //i vale a posição no vetor
 
-            lin = 
-            '<div class="polaroid">'+
-            '   <div class="pic_block">'+
-            '        <img src="../imagens/cat.jpg" class="pet_pic" style= width="200" height="200">'+
-            '    </div>'+
-            '    <div class="container">'+
-            '   <p>' + pets[i].nome + '</p>'+
-            '   </div>'+
-            '</div>';
+            lin =
+                '<div class="polaroid">' +
+                '   <div class="pic_block">' +
+                '        <img src="../imagens/cat.jpg" class="pet_pic" style= width="200" height="200">' +
+                '    </div>' +
+                '    <div class="container">' +
+                '   <p>' + pets[i].nome + '</p>' +
+                '   </div>' +
+                '</div>';
 
             // adiciona a linha no corpo da tabela
             $('#grid_pet').append(lin);
         }
     }
     // código para mapear click do botão incluir pessoa
-    $(document).on("click", "#btnSubmit", function() {
+    $(document).on("click", "#btnSubmit", function () {
+        var svalue = $('input[name=sexo]:checked', '#MyForm').val();
+        alert(svalue);
         //pegar dados da tela
         nome = $("#campoNome").val();
         idade = $("#campoIdade").val();
-        // sexo = $("#campoSexo").val();
-        // castracao = $("#campoCast").val();
-        // vacinas = $("#campoVacinas").val();
+        sexo = $("#campoSexo").val();
+        castracao = $("#campoCast").val();
+        vacinas = $("#campoVacinas").val();
         desc = $("#campoDesc").val();
         // preparar dados no formato json
-        // var dados = JSON.stringify({ nome: nome, idade: idade, sexo: sexo, castracao : castracao, vacinas: vacinas, desc: desc });
-        var dados = JSON.stringify({ nome: nome, idade: idade, desc: desc });
+        var dados = JSON.stringify({ nome: nome, idade: idade, sexo: svalue, castracao : castracao, vacinas: vacinas, desc: desc });
+        // var dados = JSON.stringify({ nome: nome, idade: idade, desc: desc });
         // fazer requisição para o back-end
         $.ajax({
             url: 'http://localhost:5000/inserir_pets',
@@ -50,25 +52,25 @@ $(function() { // quando o documento estiver pronto/carregado
             success: petInserido, // chama a função listar para processar o resultado
             error: erroAoInserir
         });
-        function petInserido (retorno) {
+        function petInserido(retorno) {
             if (retorno.resultado == "ok") { // a operação deu certo?
                 // informar resultado de sucesso
                 alert("Pet incluído com sucesso!");
                 // limpar os campos
                 $("#campoNome").val("");
                 $("#campoIdade").val("");
-                // $("#campoSexo").val("");
-                // $("#campoCast").val("");
-                // $("#campoVacinas").val("");
+                $("#campoSexo").val("");
+                $("#campoCast").val("");
+                $("#campoVacinas").val("");
                 $("#campoDesc").val("");
             } else {
                 // informar mensagem de erro
                 alert(retorno.resultado + ":" + retorno.detalhes);
-            }            
+            }
         }
-        function erroAoInserir (retorno) {
+        function erroAoInserir(retorno) {
             // informar mensagem de erro
-            alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+            alert("ERRO: " + retorno.resultado + ":" + retorno.detalhes);
         }
     });
 
